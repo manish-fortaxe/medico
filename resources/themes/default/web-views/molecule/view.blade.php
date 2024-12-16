b8c4de7d6db4e629b23ca036dc8c4135118bf472
softpromani <softpromani@gmail.com>
2024-12-16 11:27:17 +0530

    bug fixes

--- resources/themes/default/web-views/molecule/view.blade.php
+++ resources/themes/default/web-views/molecule/view.blade.php
@@ -97,13 +97,13 @@
                 <div class="card">
                   <div class="card-header" id="headingOne">
                     <h2 class="mb-0">
-                      <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne{{ $faq->id }}" aria-expanded="true" aria-controls="collapseOne{{ $faq->id }}">
                         {{ $faq->question }}
                       </button>
                     </h2>
                   </div>
 
-                  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                  <div id="collapseOne{{ $faq->id }}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                     <div class="card-body">
                       {{ $faq->answer ?? '' }}
                     </div>
